71504ba9bc43fdef6d6ab03c55a1831f25798432
Khushig11 <khushig1102@gmail.com>
2023-11-26 18:14:49 +0530

    Main page edited

--- app.py
+++ app.py
@@ -4,33 +4,33 @@ from PIL import Image, ImageTk
 
 root = Tk()
 root.title("FACE DETECTION PASSWORD MANAGER")
-root.geometry("600x400") 
+root.geometry("960x540") 
 
 
 original_image = Image.open("./bg/bg_photo.png")  
-resized_image = original_image.resize((600, 400), Image.LANCZOS)  
-background = ImageTk.PhotoImage(resized_image)
+resized_image = original_image.resize((960, 540), Image.LANCZOS)  
+back = ImageTk.PhotoImage(resized_image)
 
 my_canvas = Canvas(root)
 my_canvas.pack(fill="both", expand=True)
 
-my_canvas.create_image(0, 0, image=background, anchor="nw")
+my_canvas.create_image(0, 0, image=back, anchor="nw")
 
 style = ttk.Style()
 
 style.configure("TButton",
                 font=("Verdana", 12),
-                padding=0,
+                padding=5,
                 relief="flat",
                 background="#4ca7b3",
-                foreground="#367077")
+                foreground="#0a69a8")
 
 button1 = ttk.Button(master=my_canvas,
                      text="LOGIN")
-button1_window = my_canvas.create_window(398, 200, anchor="nw", window=button1, height=50, width=80)
+button1_window = my_canvas.create_window(635, 280, anchor="nw", window=button1, height=40, width=250)
 
-button3 = ttk.Button(master=my_canvas,
+button2 = ttk.Button(master=my_canvas,
                      text="SIGNUP")
-button3_window = my_canvas.create_window(500, 200, anchor="nw", window=button3,height=50, width=80)
+button2_window = my_canvas.create_window(635, 350, anchor="nw", window=button2,height=40, width=250)
 
 root.mainloop()
